2488e5a54623abf85f40ba92a1faba04412f448e
Ivan Adamchuk <adamchuk.v.1@gmail.com>
2021-09-04 12:10:47 +0300

    shtrudel-cafe: fix bugs

--- projects/shtrudel-cafe/js/script.js
+++ projects/shtrudel-cafe/js/script.js
@@ -1,20 +1,21 @@
-document.documentElement.style.setProperty('--vh', `${window.innerHeight/100}px`);
+document.documentElement.style.setProperty('--vh', `${window.innerHeight/100}px`); // Calculate the chrome mobile bar
 
 document.getElementById("mobile-menu-btn").addEventListener("click", function() 
 {
-  if(document.getElementById("mobile-menu-btn").classList.contains("nav-icon_open"))
-  {
-    const scrollY = document.body.style.top;
-    document.body.style.position = '';
-    document.body.style.top = '';
-    window.scrollTo(0, parseInt(scrollY || '0') * -1);
-  }
-  else
-  {  
-    document.body.style.position = 'fixed';
-    document.body.style.top = `-${window.scrollY}px`;
-  }
-
+  document.getElementById("container").classList.toggle("container_hidden");
+  document.getElementById("mobile-menu-btn").style.display = "block";
   document.getElementById("mobile-menu-btn").classList.toggle("nav-icon_open");
   document.getElementById("nav-menu").classList.toggle("menu_mobile_open");
 });
+
+for(var i = 0; i < document.getElementsByClassName("nav--link").length; i++){
+  document.getElementsByClassName("nav--link")[i].addEventListener('click', ChangeSection, false);
+}
+
+function ChangeSection()
+{
+  document.getElementById("mobile-menu-btn").classList.toggle("nav-icon_open");
+  document.getElementById("nav-menu").classList.toggle("menu_mobile_open");
+  document.getElementById("container").classList.toggle("container_hidden");
+  this.scrollIntoView();
+}
