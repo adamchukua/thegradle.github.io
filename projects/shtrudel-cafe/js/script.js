b9d59d819f01635e81fe914600d88999da56cc28
Ivan Adamchuk <adamchuk.v.1@gmail.com>
2021-11-13 21:03:12 +0200

    Strudel-cafe: js refactoring, changed images, add gallery animation

--- projects/shtrudel-cafe/js/script.js
+++ projects/shtrudel-cafe/js/script.js
@@ -1,21 +1,52 @@
-document.documentElement.style.setProperty('--vh', `${window.innerHeight/100}px`); // Calculate the chrome mobile bar
+// Calculate the chrome mobile bar
+document.documentElement.style.setProperty("--vh", `${window.innerHeight / 100}px`);
 
-document.getElementById("mobile-menu-btn").addEventListener("click", function() 
-{
-  document.getElementById("container").classList.toggle("container_hidden");
-  document.getElementById("mobile-menu-btn").style.display = "block";
-  document.getElementById("mobile-menu-btn").classList.toggle("nav-icon_open");
-  document.getElementById("nav-menu").classList.toggle("menu_mobile_open");
+/////// MOBILE MENU ///////
+
+const container = document.querySelector("#container");
+const menu = document.querySelector("#nav-menu");
+const btn = document.querySelector("#mobile-menu-btn");
+const links = document.querySelectorAll(".nav--link");
+
+btn.addEventListener("click", function() {
+  container.classList.toggle("container_hidden");
+  btn.style.display = "block";
+  btn.classList.toggle("nav-icon_open");
+  menu.classList.toggle("menu_mobile_open");
 });
 
-for(var i = 0; i < document.getElementsByClassName("nav--link").length; i++){
-  document.getElementsByClassName("nav--link")[i].addEventListener('click', ChangeSection, false);
-}
+links.forEach(link => {
+  link.addEventListener("click", ChangeSection, false);
+});
 
-function ChangeSection()
-{
-  document.getElementById("mobile-menu-btn").classList.toggle("nav-icon_open");
-  document.getElementById("nav-menu").classList.toggle("menu_mobile_open");
-  document.getElementById("container").classList.toggle("container_hidden");
+function ChangeSection() {
+  btn.classList.toggle("nav-icon_open");
+  menu.classList.toggle("menu_mobile_open");
+  container.classList.toggle("container_hidden");
   this.scrollIntoView();
 }
+
+/////// GALLERY ///////
+
+const images = document.querySelectorAll(".gallery-imgs > img");
+let isPreview = false;
+
+images.forEach(image => {
+  image.addEventListener("click", function(event) {
+    if (isPreview) {
+      isPreview = false;
+
+      images.forEach(unclickedImage => {
+        unclickedImage.style.width = "calc(50% - 2.5px)";
+      });
+    } else {
+      isPreview = true;
+
+      images.forEach(unclickedImage => {
+        unclickedImage.style.width = "0%";
+      });
+  
+      event.target.style.width = "100%";
+    }
+  });
+});
