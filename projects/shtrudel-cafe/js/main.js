c31ce65f6ed4e2ff4c0ad61b76ec454097f92e40
Ivan Adamchuk <adamchuk.v.1@gmail.com>
2022-01-17 23:44:26 +0200

    Add menu

---
+++ projects/shtrudel-cafe/js/main.js
@@ -0,0 +1,27 @@
+// Calculate the chrome mobile bar
+document.documentElement.style.setProperty("--vh", `${window.innerHeight / 100}px`);
+
+/////// MOBILE MENU ///////
+
+const container = document.querySelector("#container");
+const menu = document.querySelector("#nav-menu");
+const btn = document.querySelector("#mobile-menu-btn");
+const links = document.querySelectorAll(".nav--link");
+
+btn.addEventListener("click", function() {
+  container.classList.toggle("container_hidden");
+  btn.style.display = "block";
+  btn.classList.toggle("nav-icon_open");
+  menu.classList.toggle("menu_mobile_open");
+});
+
+links.forEach(link => {
+  link.addEventListener("click", ChangeSection, false);
+});
+
+function ChangeSection() {
+  btn.classList.toggle("nav-icon_open");
+  menu.classList.toggle("menu_mobile_open");
+  container.classList.toggle("container_hidden");
+  this.scrollIntoView();
+}
--- projects/shtrudel-cafe/js/menu.js
+++ projects/shtrudel-cafe/js/menu.js
@@ -1,33 +1,39 @@
-/////// DATA ///////
-
-const menuDict = {};
-menuDict["ТАКО З ЯЛОВИЧИНИ\n900₴"] = ["takozyalovichini.jpg", "ТАКО З ЯЛОВИЧИНИ", "texttexttexttexttexttext", "900₴"];
-menuDict["ГОСТРІ ТАКО З КРЕВЕТКАМИ\n900₴"] = ["other.jpg", "ГОСТРІ ТАКО З КРЕВЕТКАМИ", "21323213123213123123", "900₴"];
-
 const menuList = document.querySelectorAll("li");
+const menuBtns = document.querySelectorAll("li img");
 const modalInfo = document.querySelector(".modal-info");
+const closeBtn = document.querySelector(".close");
 const dishImg = document.querySelector(".modal-info-content__img");
 const dishTitle = document.querySelector(".modal-info-content__title");
 const dishDesc = document.querySelector(".modal-info-content__desc");
 const dishPrice = document.querySelector(".modal-info-content__price");
 
+function ShowInfo(name) {
+  modalInfo.style.opacity = "1";
+  modalInfo.style.visibility = "visible";
+  
+  /*console.log("1" + name);
+  if (!(name in menuDict)) {
+    modalInfo.style.opacity = "0";
+    modalInfo.style.visibility = "hidden";
+    console.log("2" + name);
+    return;
+  }*/
+
+  dishImg.src = "img/menu/" + menuDict[name][0];
+  dishTitle.innerText = menuDict[name][1];
+  dishDesc.innerText = menuDict[name][2];
+  dishPrice.innerText = menuDict[name][3];
+}
+
+menuBtns.forEach(item => {
+  item.addEventListener("click", function() {
+    ShowInfo(event.path[1].innerText);
+  });
+});
+
 menuList.forEach(item => {
   item.addEventListener("click", function() {
-    modalInfo.style.opacity = "1";
-    modalInfo.style.visibility = "visible";
-
-    let dish = event.target.innerText;
-
-    if (!(dish in menuDict)) {
-      modalInfo.style.opacity = "0";
-      modalInfo.style.visibility = "hidden";
-      return;
-    }
-
-    dishImg.src = "img/menu/" + menuDict[dish][0];
-    dishTitle.innerText = menuDict[dish][1];
-    dishDesc.innerText = menuDict[dish][2];
-    dishPrice.innerText = menuDict[dish][3];
+    ShowInfo(event.target.innerText);
   });
 });
 
